bcb3b39b3c4f7954069ace958cc3f43a0e79301b
jaquew <eng.jaquesilva@gmail.com>
2020-09-04 21:59:18 -0300

    pag evolucao

--- src/pages/evolucao.js
+++ src/pages/evolucao.js
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 
 function Evolucao() {
+  const [active, setActive] = useState(true)
+
+
   return(
   <section>
     <h1>Página Evoluçao</h1>
+    <div className="tabMenu">
+      <button className={active ? "activeTab" : "tab"} onClick={()=> setActive(active => !active)}>Gráfico</button>
+      <button className={!active ? "activeTab" : "tab"} onClick={()=>setActive(active => !active)}>Resgate</button>
+    </div> 
+    {active ? 
+    <p>oi</p>
+    :
+    <p>tchau</p>}
   
   </section>
 
