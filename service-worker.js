8a64baaa895692b879d9b8f313856481bf48276b
jaquew <eng.jaquesilva@gmail.com>
2020-09-07 20:03:43 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/financas/precache-manifest.095f0874fd1f0f016f5c31f52544021a.js"
+  "/financas/precache-manifest.63d48ec0d2df9ea579b914f54f781dd8.js"
 );
 
 self.addEventListener('message', (event) => {
